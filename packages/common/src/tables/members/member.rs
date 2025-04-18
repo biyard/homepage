52999e96674e0ebb5ea6ae1dd8f2fa78067884da
hackartists <hackartists@gmail.com>
2025-04-18 16:09:56 +0900

    *** fix(member): correct English translation for Researcher role

--- packages/common/src/tables/members/member.rs
+++ packages/common/src/tables/members/member.rs
@@ -41,6 +41,6 @@ pub enum MemberRole {
     Designer = 4,
     #[translate(ko = "소프트웨어 엔지니어", en = "Software Engineer")]
     Developer = 5,
-    #[translate(ko = "연구원", en = "Research")]
+    #[translate(ko = "연구원", en = "Researcher")]
     Researcher = 6,
 }
